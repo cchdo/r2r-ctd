0b32921df3384fa39242f3441ad1c5f5e12fe541
Andrew Barna <abarna@gmail.com>
2025-07-02 22:45:17 -0700

    add file presence and valid checksum xml report funcs

--- src/r2r_ctd/reporting.py
+++ src/r2r_ctd/reporting.py
@@ -1,4 +1,7 @@
+from typing import Literal
+
 from lxml.builder import ElementMaker
+from lxml.etree import ElementTree
 
 E = ElementMaker(
     namespace="https://service.rvdata.us/schema/r2r-2.0",
@@ -25,3 +28,27 @@ rating = {
         "X if one or more tests could not be run."
     )
 }
+
+
+def file_presence(rating: Literal["G", "R"], test_result: str | int) -> ElementTree:
+    """Construct the ElementTree for the all raw files test"""
+    return Test(
+        Rating(rating),
+        TestResult(str(test_result), uom="Percent"),
+        Bounds(Bound("100", name="MinimumPercentToPass", uom="Percent")),
+        description="GREEN if 100% of the casts have .hex/.dat, .con and .hdr files; else RED",
+        name="Presence of All Raw Files",
+    )
+
+
+def valid_checksum(rating: Literal["G", "R"]) -> ElementTree:
+    """Construct the ElementTree for the valid checksum test"""
+
+    return Test(
+        Rating(rating),
+        Bounds(
+            Bound("True/False", name="AllFilesHaveValidChecksum", uom="Unitless"),
+        ),
+        description="GREEN if 100% of the files in the manifest have valid checksums; else RED",
+        name="Valid Checksum for All Files in Manifest",
+    )
