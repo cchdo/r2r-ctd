74fe3eff4bb6f1935ef8b052dcdc977de51f5e6b
Andrew Barna <abarna@gmail.com>
2025-07-22 23:50:37 -0700

    move the cli qa to a subcommand

--- src/r2r_ctd/__main__.py
+++ src/r2r_ctd/__main__.py
@@ -10,15 +10,18 @@ import click
 
 from r2r_ctd.state import get_xml_qa_path
 
+@click.group()
+def cli(): ...
 
-@click.command()
+@cli.command()
 @click.argument(
     "paths",
     nargs=-1,
     required=True,
     type=click.Path(exists=True, file_okay=False, writable=True, path_type=Path),
 )
-def main(paths: tuple[Path, ...]):
+def qa(paths: tuple[Path, ...]):
+    """Run the QA routines on one or more directories"""
     FORMAT = "%(message)s"
     logging.basicConfig(
         level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
@@ -29,13 +32,14 @@ def main(paths: tuple[Path, ...]):
         qa_xml = breakout.qa_template_xml
 
         ra = ResultAggregator(breakout)
+        certificate = ra.certificate
         ra.gen_cnvs()
 
         root = qa_xml.getroot()
         nsmap = root.nsmap
         prefix = "/r2r:qareport"
         cert = root.xpath(f"{prefix}/r2r:certificate", namespaces=nsmap)[0]
-        root.replace(cert, ra.certificate)
+        root.replace(cert, certificate)
         with open(get_xml_qa_path(breakout), "wb") as f:
             qa_xml.write(
                 f,
@@ -47,4 +51,4 @@ def main(paths: tuple[Path, ...]):
 
 
 if __name__ == "__main__":
-    main()
+    cli()
